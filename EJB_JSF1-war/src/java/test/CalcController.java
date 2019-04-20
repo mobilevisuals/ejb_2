1892237018e9dae4e4b2bc2aa99e608030baae4f
eyvind <eyvind@192.168.0.106>
2019-04-20 14:41:19 +0200

    sdfsf

---
+++ EJB_JSF1-war/src/java/test/CalcController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author eyvind
+ */
+
+@Named(value = "calcController")
+@SessionScoped
+public class CalcController implements Serializable {
+    
+    private float a,b,result;
+    
+
+    @EJB
+    private calcBeanLocal calcBean;
+
+    /**
+     * Creates a new instance of CalcController
+     */
+    public CalcController() {
+    }
+
+    public float getA() {
+        return a;
+    }
+
+    public void setA(float a) {
+        this.a = a;
+    }
+
+    public float getB() {
+        return b;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+
+    public float getResult() {
+        return result;
+    }
+
+    public void setResult(float result) {
+        this.result = result;
+    }
+    
+    
+    
+    public void add()
+    {
+    result=calcBean.add(a,b);
+    result=calcBean.add(a,b);
+    }
+    
+}
